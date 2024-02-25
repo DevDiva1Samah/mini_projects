fc9428d2b3c3b33dc18d291ab0751b87a7f25d1d
DevDiva1Samah <158510060+DevDiva1Samah@users.noreply.github.com>
2024-02-24 21:12:16 -0500

    to do from scratch

--- Hangman_from-scratch.py
+++ Hangman_from-scratch.py
@@ -1,15 +1,30 @@
 import random
 import words
-import string
+import string 
 
 
-
-def guess_valid_word(words):
+def get_valid_word(words):   #here where we choosed the secret word
 
     word = random.choice(words)
-    user = input("guess a valid word that exists in words").upper()
-
-    while (' ' or ',') in words:
+    
+    while (' ' or ',') in words:  #white and underscores
         return word
     
-def 
+def Hangman():
+
+    word = get_valid_word(words)   #like here we just call our function
+    letters = set(words)
+    alaphabet = set(string.ascii_uppercase(words))
+    user_letter = ()
+
+    #implement a loop
+    while user_guess_letter:    #to do from scratch again!!!!
+
+
+
+
+
+
+
+    f"you have these lives {} and you spent all you chances"
+   
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-#using string concatenation 
+import string
+import random
+
+print(help(random))
+
+
+
+
+#we have a liste of words
+#hangman is a game where you have a list of words 
+#we choose a secret word and the player have to guess it
